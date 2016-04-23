c493f24fe316a05fc3612a55cff8570f5cc76a64
Patrick Feltes <pj6444@gmail.com>
2016-04-23 00:51:47 -0500

    frame skeleton

---
+++ src/com/patrickfeltes/main/GamePanel.java
@@ -0,0 +1,108 @@
+package com.patrickfeltes.main;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+public class GamePanel extends JPanel implements Runnable, MouseListener, MouseMotionListener {
+
+    // dimensions
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 800;
+    public static final Dimension size = new Dimension(WIDTH, HEIGHT);
+
+    // mouse position
+    public static int mouseX, mouseY;
+
+    // Thread
+    private Thread thread;
+    private int FPS = 60;
+    private long targetTime = 1000 / FPS;
+    private boolean isRunning = false;
+
+    // Graphics
+    private Graphics2D g2d;
+
+    // constructor
+    public GamePanel() {
+        setPreferredSize(size);
+        setMinimumSize(size);
+        setMaximumSize(size);
+        startGame();
+    }
+
+    public void update() {
+
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        // antialiasing to have decent looking graphics
+        g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+    }
+
+    // main loop for the game
+    public void run() {
+        long start, elapsed, wait;
+        while(isRunning) {
+            start = System.currentTimeMillis();
+
+            // update and draw
+            update();
+            repaint();
+
+            elapsed = System.currentTimeMillis() - start;
+            wait = targetTime - elapsed;
+            if(wait < 0) { wait = 5; }
+            try {
+                Thread.sleep(wait);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        endGame();
+    }
+
+    // events
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    public void mouseDragged(MouseEvent e) {
+        setMousePosition(e.getX(), e.getY());
+    }
+
+    public void mouseMoved(MouseEvent e) {
+        setMousePosition(e.getX(), e.getY());
+    }
+
+    public void mousePressed(MouseEvent e) {}
+    public void mouseReleased(MouseEvent e) {}
+    public void mouseEntered(MouseEvent e) {}
+    public void mouseExited(MouseEvent e) {}
+
+    // helper methods
+    private void startGame() {
+        isRunning = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    private void endGame() {
+        isRunning = false;
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setMousePosition(int x, int y) {
+        mouseX = x;
+        mouseY = y;
+    }
+}
--- src/com/patrickfeltes/main/TicTacToe.java
+++ src/com/patrickfeltes/main/TicTacToe.java
@@ -1,7 +1,20 @@
 package com.patrickfeltes.main;
 
-/**
- * Created by Patrick on 4/23/2016.
- */
+
+import javax.swing.*;
+import java.awt.*;
+
 public class TicTacToe {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setLayout(new FlowLayout());
+        frame.add(new GamePanel());
+        frame.pack();
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+    }
+
 }
