fb255f40a60d3c9264a36248c0d0d0d15100ad3b
Patrick Feltes <pj6444@gmail.com>
2016-04-25 20:37:17 -0500

    functioning visual/AI works?

--- src/com/patrickfeltes/game/Board.java
+++ src/com/patrickfeltes/game/Board.java
@@ -1,29 +1,159 @@
 package com.patrickfeltes.game;
 
+import java.awt.*;
+import java.util.ArrayList;
+
 public class Board {
 
     private int size;
-    private boolean isAIOn;
 
     private final int AI = -1;
     private final int EMPTY = 0;
     private final int USER = 1;
 
+    private int currentPlayer;
+
     private int[][] board;
 
-    public Board(int size, boolean isAIOn) {
+    public Board(int size) {
         this.size = size;
-        this.isAIOn = isAIOn;
         board = new int[size][size];
     }
 
+    private ArrayList<Point> getPossibleMoves() {
+        ArrayList<Point> moves = new ArrayList<Point>();
+
+        for(int r = 0; r < size; r++) {
+            for(int c = 0; c < size; c++) {
+                if(board[r][c] == EMPTY) { moves.add(new Point(c, r)); }
+            }
+        }
+
+        return moves;
+    }
+
+    // using the minimax method, make the best move for the AI player
+    public void makeAIMove() {
+        ArrayList<Point> possibleMoves = getPossibleMoves();
+
+        Point minMove = null;
+        int min = Integer.MAX_VALUE;
+
+        for(Point move: possibleMoves) {
+            makeMove(move, AI);
+
+            int val = minimax(USER);
+            if(val < min) {
+                min = val;
+                minMove = move;
+            }
+
+            undoMove(move);
+        }
+        makeMove(minMove, AI);
+        switchPlayer();
+    }
 
 
+    private int minimax(int player) {
+        if(hasWon(AI) || hasWon(USER)) { return score(); }
+
+        ArrayList<Point> possibleMoves = getPossibleMoves();
+
+        if(possibleMoves.size() == 0) { return score(); }
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for(Point move: possibleMoves) {
+            if(player == USER) {
+                makeMove(move, player);
+                int stateScore = minimax(AI);
+                max = Math.max(stateScore, max);
+            } else {
+                makeMove(move, player);
+                int stateScore = minimax(USER);
+                min = Math.min(stateScore, min);
+            }
+            undoMove(move);
+        }
+
+        return player == USER ? max : min;
+    }
+
+    private int score() {
+        if(hasWon(AI)) {
+            return AI * 10;
+        } else if(hasWon(USER)) {
+            return USER * 10;
+        } else {
+            return 0;
+        }
+    }
+
+    private void makeMove(Point move, int player) {
+        board[move.y][move.x] = player;
+    }
+
+    private void undoMove(Point move) {
+        board[move.y][move.x] = EMPTY;
+    }
+
+    private void switchPlayer() {
+        currentPlayer = (currentPlayer == USER) ? AI : USER;
+    }
+
+    private boolean hasWon(int player) {
+        if(checkRows(player)) return true;
+        else if(checkCols(player)) return true;
+        else if(checkDiagonals(player)) return true;
+        return false;
+    }
+
+    // helper methods for hasWon
+    private boolean checkRows(int player) {
+        for(int r = 0; r < size; r++) {
+            if(allSame(board[r], player)) return true;
+        }
+        return false;
+    }
+
+    private boolean checkCols(int player) {
+        int[] tempCol = new int[size];
+
+        for(int c = 0; c < size; c++) {
+            for(int r = 0; r < size; r++) {
+                tempCol[r] = board[r][c];
+            }
+            if(allSame(tempCol, player)) { return true; }
+        }
+
+        return false;
+    }
+
+    private boolean checkDiagonals(int player) {
+        int[] a1 = new int[size];
+        int[] a2 = new int[size];
+
+        for(int r = 0; r < size; r++) {
+            a1[r] = board[r][r];
+            a2[r] = board[r][size - r - 1];
+        }
+
+        return (allSame(a1, player) || allSame(a2, player));
+    }
+
+    private boolean allSame(int[] array, int value) {
+        for(int i = 0; i < array.length; i++) {
+            if(array[i] != value) { return false; }
+        }
+        return true;
+    }
 
 
     public int[][] getBoard() {
         return board;
     }
-
+    public int getCurrentPlayer() { return currentPlayer; }
 
 }
--- src/com/patrickfeltes/gamestates/OnePlayerState.java
+++ src/com/patrickfeltes/gamestates/OnePlayerState.java
@@ -1,20 +1,42 @@
 package com.patrickfeltes.gamestates;
 
+import com.patrickfeltes.game.Board;
+import com.patrickfeltes.main.GamePanel;
+
 import java.awt.*;
 import java.awt.event.MouseEvent;
 
 public class OnePlayerState extends GameState {
 
+    private final int CELLS = 3;
+
+    private final int BORDER = (int)(0.10 * GamePanel.WIDTH);
+    private final int CELL_SIZE = (GamePanel.WIDTH - 2 * BORDER) / CELLS;
+
+    private int row, col;
+
     public OnePlayerState(GameStateManager gsm) {
         super(gsm);
+        Board b = new Board(CELLS);
     }
 
     public void update() {
-
+        row = (GamePanel.mouseY - BORDER) / CELL_SIZE;
+        col = (GamePanel.mouseX - BORDER) / CELL_SIZE;
     }
 
     public void draw(Graphics2D g) {
+        // draw the board
+        g.setColor(Color.BLACK);
+        for(int i = 1; i <= CELLS - 1; i++) {
+            g.drawLine(BORDER + CELL_SIZE * i, BORDER, BORDER + CELL_SIZE * i, GamePanel.HEIGHT - BORDER);
+            g.drawLine(BORDER, BORDER + CELL_SIZE * i, GamePanel.WIDTH - BORDER, BORDER + CELL_SIZE * i);
+        }
 
+        if(row >= 0 && row < CELLS && col >= 0 && col < CELLS) {
+            g.setColor(new Color(0.5f,0.5f,0.5f,0.5f));
+            g.fillRect(BORDER + CELL_SIZE * col, BORDER + CELL_SIZE * row, CELL_SIZE, CELL_SIZE);
+        }
     }
 
     public void mouseClicked(MouseEvent e) {
